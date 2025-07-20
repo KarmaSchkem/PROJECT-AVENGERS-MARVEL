f9e58555bc4012a060db6f10d6d78843f6351db3
KarmaSchkem <151461062+KarmaSchkem@users.noreply.github.com>
2025-07-20 14:40:36 +0200

    Mise à jour pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,81 @@
- 
+namespace SpriteKind {
+    export const loc = SpriteKind.create()
+    export const Map = SpriteKind.create()
+}
+browserEvents.Shift.onEvent(browserEvents.KeyEvent.Pressed, function () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Map)
+    myMinimap = minimap.minimap(MinimapScale.Half, 1, 15)
+    minimap.includeSprite(myMinimap, sprite_loc, MinimapSpriteScale.MinimapScale)
+    mapSprite = sprites.create(minimap.getImage(myMinimap), SpriteKind.Map)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
+    if (browserEvents.E.isPressed()) {
+        tiles.setCurrentTilemap(tilemap`niveau2`)
+        scene.setBackgroundColor(12)
+    }
+})
+browserEvents.Shift.onEvent(browserEvents.KeyEvent.Released, function () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Map)
+    scene.cameraFollowSprite(mySprite)
+})
+function boot () {
+    scene.setBackgroundColor(7)
+    tiles.setCurrentTilemap(tilemap`niveau1`)
+    sprite_loc = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . 2 2 2 2 . . . . . . 
+        . . . . 2 2 2 f f 2 2 2 . . . . 
+        . . . . 2 2 2 f f 2 2 2 . . . . 
+        . . . . . . 2 2 2 2 . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.loc)
+    mySprite = sprites.create(img`
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . e e e e e e . . . . . 
+        . . . . . b f e e f b . . . . . 
+        . . . . . 1 f e e f 1 . . . . . 
+        . . . . . e e e e e e . . . . . 
+        . . . . . . e e e e . . . . . . 
+        . . . . 8 8 8 8 8 8 8 8 . . . . 
+        . . . 8 8 8 8 8 8 8 8 8 8 . . . 
+        . . . 8 8 8 8 8 8 8 8 8 8 . . . 
+        . . . 8 8 8 8 8 8 8 8 8 8 . . . 
+        . . . e e 4 4 5 5 4 4 e e . . . 
+        . . . e . b b b b b b . e . . . 
+        . . . . . b b b b b b . . . . . 
+        . . . . . b b . . b b . . . . . 
+        . . . . . c c . . c c . . . . . 
+        `, SpriteKind.Player)
+    sprite_loc.follow(mySprite, 100)
+    controller.moveSprite(mySprite)
+    tiles.placeOnRandomTile(sprite_loc, assets.tile`myTile3`)
+    tiles.placeOnRandomTile(mySprite, assets.tile`myTile3`)
+    scene.cameraFollowSprite(mySprite)
+}
+let mySprite: Sprite = null
+let mapSprite: Sprite = null
+let sprite_loc: Sprite = null
+let myMinimap: minimap.Minimap = null
+namespace userconfig {
+    export const ARCADE_SCREEN_WIDTH = 320
+    export const ARCADE_SCREEN_HEIGHT = 240
+}
+boot()
+game.onUpdateInterval(10, function () {
+    if (browserEvents.Shift.isPressed()) {
+        myMinimap = minimap.minimap(MinimapScale.Half, 1, 15)
+        minimap.includeSprite(myMinimap, sprite_loc, MinimapSpriteScale.MinimapScale)
+        mapSprite.setImage(minimap.getImage(myMinimap))
+    }
+})
---
+++ tilemap.g.ts
@@ -0,0 +1,125 @@
+// Code généré automatiquement. Ne pas modifier.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile18 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile19 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "niveau1":
+            case "niveau1":return tiles.createTilemap(hex`100010000402020202020a020202020202020207050101010101050910091009100910050509100910090e1009100910091009050302020f02020b0910091009100910050509100910090510091009100910090505100910091005091009100910091005050910091009030202020202020f020b051009100910050101010101011001050509100910090e09100910091009100505100910091005010d100101010101050302020f02020b091209100910091005051009100910050101010101090101050509100910090e09100910091009100505100911091005100101010101010105050910091009050910091009100910050602020202020c020202020202020208`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 2 . 2 2 2 . . . . . . . . 2 
+2 . . . . . 2 . . . . . . . . 2 
+2 . . . . . 2 . . . . . . . . 2 
+2 . . . . . 2 2 2 2 2 2 2 . 2 2 
+2 . . . . . 2 2 2 2 2 2 2 . 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . 2 2 2 . 2 2 2 2 2 2 
+2 2 2 . 2 2 2 . . . . . . . . 2 
+2 . . . . . 2 2 2 2 2 2 . 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . 2 . 2 2 2 2 2 2 2 2 
+2 . . . . . 2 . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,myTiles.tile1,sprites.vehicle.roadHorizontal,sprites.vehicle.roadIntersection2,sprites.vehicle.roadTurn1,sprites.vehicle.roadVertical,sprites.vehicle.roadTurn3,sprites.vehicle.roadTurn2,sprites.vehicle.roadTurn4,sprites.castle.tileGrass1,sprites.vehicle.roadIntersection3,sprites.vehicle.roadIntersection4,sprites.vehicle.roadIntersection1,myTiles.tile2,myTiles.tile3,myTiles.tile4,sprites.castle.tileGrass3,myTiles.tile5,myTiles.tile6], TileScale.Sixteen);
+            case "niveau2":
+            case "niveau2":return tiles.createTilemap(hex`14000f000401010101010101010101010101010101010105060b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b06060b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b06060b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b06060b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b06060b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b06060b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b06060b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b06060b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b06060b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b06060b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b06060b08050b0b0b0b0b0b0b0b0b0b0b0b0b0b0b06060b0b060b0b0b0b0b0b0b0b0b0b0b0b0b0b0b06060b0b060b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0602090a0701010101010101010101010101010103`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . 2 
+2 . 2 2 . . . . . . . . . . . . . . . 2 
+2 . . 2 . . . . . . . . . . . . . . . 2 
+2 . . 2 . . . . . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11,myTiles.tile12,myTiles.tile13,myTiles.tile14,myTiles.tile15,myTiles.tile18,myTiles.tile19], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile4":
+            case "tile6":return tile6;
+            case "myTile5":
+            case "tile7":return tile7;
+            case "myTile6":
+            case "tile8":return tile8;
+            case "myTile7":
+            case "tile9":return tile9;
+            case "myTile8":
+            case "tile10":return tile10;
+            case "myTile9":
+            case "tile11":return tile11;
+            case "myTile10":
+            case "tile12":return tile12;
+            case "myTile11":
+            case "tile13":return tile13;
+            case "myTile12":
+            case "tile14":return tile14;
+            case "myTile13":
+            case "tile15":return tile15;
+            case "myTile16":
+            case "tile18":return tile18;
+            case "myTile17":
+            case "tile19":return tile19;
+        }
+        return null;
+    })
+
+}
+// Code généré automatiquement. Ne pas modifier.
